715d7fba2b21414dba3af2b40f25ec15fce11677
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2021-11-23 21:55:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+song1 = "";
+song2 = "";
+
+function preload(){
+    song1 = loadSound("americanmusic.mp3");
+    song2 = loadSound("russianmusic.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
